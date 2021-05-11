c1fac13c657bee08442f668930992be5c7e4031b
Vince Tita <vince.tita@gmail.com>
2021-05-11 14:48:36 +0200

    chapter3 commit

--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,37 @@
-const character = 'mario';
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
 
-console.log(character);
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
 
-const inputs = document.querySelectorAll('input');
+let numbers = [10, 20, 12, 15];
 
-inputs.forEach(input => {
-  console.log(input);
-});
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+  // skills: ['running'],
+};
