6a249d1e024739866e7f6766c12bbf2632f4d42e
Vince Tita <vince.tita@gmail.com>
2021-05-11 23:43:05 +0200

    chapter5 commited

--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,24 @@
-const character = 'mario';
+let age: any = 25;
 
-console.log(character);
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'luigi' };
+console.log(age);
 
-const inputs = document.querySelectorAll('input');
+let mixed: any[] = [];
 
-inputs.forEach(input => {
-  console.log(input);
-});
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
+console.log(mixed);
+
+let ninja: { name: any, age: any };
+
+ninja = { name: 'yoshi', age: 25 };
+console.log(ninja);
+
+ninja = { name: 25, age: 'yoshi' };
+console.log(ninja);
+
