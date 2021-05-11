c5d2817df7b755e4be80ffcb542e9b0a70ad8284
Vince Tita <vince.tita@gmail.com>
2021-05-11 23:35:27 +0200

    chapter commited

--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,36 @@
-const character = 'mario';
+let character: string = 'mario';
+let age: number;
+let isLoggedIn: boolean;
 
-console.log(character);
+// age = 'luigi';
+age = 30;
 
-const inputs = document.querySelectorAll('input');
+// isLoggedIn = 25;
+isLoggedIn = true;
 
-inputs.forEach(input => {
-  console.log(input);
-});
+// arrays
+let ninjas: string[] = [];
+
+ninjas.push('ryu');
+ninjas.push('chun-li');
+console.log(ninjas);
+
+// union types
+let mixed: (string|number|boolean)[] = [];
+mixed.push('hello');
+mixed.push(false);
+mixed.push(20);
+console.log(mixed);
+
+let uid: string|number;
+
+// objects
+let ninjaOne: object;
+ninjaOne = { name: 'yoshi', age: 30 };
+
+let ninjaTwo: {
+  name: string,
+  age: number,
+  beltColour: string
+};
+ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
